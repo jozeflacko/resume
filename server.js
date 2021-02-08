2a1f222ee0eedff37599949ccfb7d5103206903d
Jozef Lacko <jlacko27@gmail.com>
2021-02-08 20:54:10 +0100

    trying to build from root

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.static(__dirname + '/build'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.listen(port, function () {
+    console.log(`Example app listening on port !`);
+});
---
+++ updatePathsInIndexFileToPointToBuildDirectory.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+
+var someFile = 'index.html';
+
+fs.readFile(someFile, 'utf8', function (err,data) {
+    if (err) {
+        return console.log(err);
+    }
+    var result = data.replace('="/', '="/build');
+
+    fs.writeFile(someFile, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+    });
+});
