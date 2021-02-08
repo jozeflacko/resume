24c3404476f5606605b548811f29787453c33124
Jozef Lacko <jlacko27@gmail.com>
2021-02-08 21:06:18 +0100

    starting from root on github

--- updatePathsInIndexFileToPointToBuildDirectory.js
+++ updatePathsInIndexFileToPointToBuildDirectory.js
@@ -6,8 +6,7 @@ fs.readFile(someFile, 'utf8', function (err,data) {
     if (err) {
         return console.log(err);
     }
-    var result = data.replace('="/', '="/build');
-
+    var result = data.split('="/').join('="/build/');
     fs.writeFile(someFile, result, 'utf8', function (err) {
         if (err) return console.log(err);
     });
