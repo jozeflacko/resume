428074700c561f434247109b7a71263c66731741
Jozef Lacko <jlacko27@gmail.com>
2021-02-08 12:52:42 +0100

    update link to point to drive

--- src/texts/links.ts
+++ src/texts/links.ts
@@ -13,7 +13,7 @@ export const Links: Array<ILink> = [
     title:'Look below to get to know me better',
   },
   {
-    name:'Download Resume',
+    name:'Go to Resume',
     // view only link to drive
     url:'https://docs.google.com/document/d/14LYgHpDHqbICLIP2PR2bUVdlP2QYOa_xBrMAM9U05ec/edit?usp=sharing',
     title:'Download Resume as PDF'
